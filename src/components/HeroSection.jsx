a11d702d6ea40b64d18beb81ffe8722a3f5b83e6
akhilbandaruu <akhilbandaru@gmail.com>
2024-08-07 22:23:00 +0530

    Changes in UI and images

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,89 +1,111 @@
-import { useEffect } from "react";
+import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 
 export default function HeroSection({ elementsRef }) {
-  function customCursor() {
-    document.body.classList.add("custom-cursor");
-  }
+  useEffect(() => {
+    const handleCustomCursor = () =>
+      document.body.classList.add('custom-cursor');
+    const handleDefaultCursor = () =>
+      document.body.classList.remove('custom-cursor');
 
-  function defaultCursor() {
-    document.body.classList.remove("custom-cursor");
-  }
+    const element = elementsRef.current[0];
+    if (element) {
+      element.addEventListener('mouseover', handleCustomCursor);
+      element.addEventListener('mouseleave', handleDefaultCursor);
+
+      return () => {
+        element.removeEventListener('mouseover', handleCustomCursor);
+        element.removeEventListener('mouseleave', handleDefaultCursor);
+      };
+    }
+  }, [elementsRef]);
+
+  const handleClick = () => {
+    window.location.href = '#index';
+  };
 
   return (
-    <div
-      data-name=""
-      onMouseOver={customCursor}
-      onMouseLeave={defaultCursor}
-      onClick={() => {
-        window.location.href = "#index";
-      }}
+    <section
       ref={(el) => (elementsRef.current[0] = el)}
-      className="hero-section">
-      <div className="hero-profile-row">
+      className='hero-section'
+      onClick={handleClick}
+    >
+      <header className='hero-profile-row'>
         <div>
-          <div className="profile-name-design">
-            <div className="round">
+          <div className='profile-name-design'>
+            <div className='round'>
               <div></div>
             </div>
-            <div className="line"></div>
+            <div className='line'></div>
           </div>
         </div>
         <div>
           <h1
-            data-option="strip-slide-up"
-            className="portfolio-title animate strip-slide-up">
+            data-option='strip-slide-up'
+            className='portfolio-title animate strip-slide-up'
+          >
             AKHIL BANDARU <br /> CREATIVE DESIGNER
           </h1>
-          <div className="portfolio-title-design">
+          <div className='portfolio-title-design'>
             <div></div>
             <div></div>
             <div></div>
-            <div className="portfolio-timeline">
+            <div className='portfolio-timeline'>
               <p>BORN – 03 NOV 1989</p>
               <p>2024</p>
             </div>
           </div>
         </div>
-      </div>
-      <div className="hero-nav-row">
-        <div className="flip-word-col">
+      </header>
+      <nav className='hero-nav-row'>
+        <article className='flip-word-col'>
           <h2
-            data-option="strip-slide-up"
-            className="flip-word-heading animate strip-slide-up">
+            data-option='strip-slide-up'
+            className='flip-word-heading animate strip-slide-up'
+          >
             LET THE
             <br />
             MAGIC UNFOLD!
           </h2>
           <p
-            data-option="fade-in"
-            className="fade-out flip-word-sub-heading animate">
+            data-option='fade-in'
+            className='fade-out flip-word-sub-heading animate'
+          >
             HELLO VISITOR!
           </p>
           <p
-            data-option="fade-in"
-            className="fade-out flip-word-content animate">
-            Howdy, my friend <i>(yes, I'll call you that henceforth).</i> Thanks
-            for stopping by my website, well, this is actually much more than
-            just a website.
+            data-option='fade-in'
+            className='fade-out flip-word-content animate'
+          >
+            Howdy, my friend <i>(yes, I&apos;ll call you that henceforth).</i>{' '}
+            Thanks for stopping by my website, well, this is actually much more
+            than just a website.
           </p>
           <p
-            data-option="fade-in"
-            className="fade-out flip-word-content animate">
-            Consider this to be a portal{" "}
-            <i>(you guessed it, I've watched too many sci-fi movies)</i> to dive
-            into my world. A world of design possibilities, a place of magic,
-            and in this realm, I'm the magician with the magic wand{" "}
+            data-option='fade-in'
+            className='fade-out flip-word-content animate'
+          >
+            Consider this to be a portal
+            <i>(you guessed it, I&apos;ve watched too many sci-fi movies)</i> to
+            dive into my world. A world of design possibilities, a place of
+            magic, and in this realm, I&apos;m the magician with the magic wand
             <i>(or a ring, your imaginative choice)!</i>
           </p>
-        </div>
-        {/* <div className="navigation">
+        </article>
+        {/* Uncomment this if navigation is needed
+        <div className="navigation">
           <a href="#index">
             <div>
-              <img src="/nav-arrow.svg" />
+              <img src="/nav-arrow.svg" alt="Navigation Arrow" />
             </div>
           </a>
-        </div> */}
-      </div>
-    </div>
+        </div>
+        */}
+      </nav>
+    </section>
   );
 }
+
+HeroSection.propTypes = {
+  elementsRef: PropTypes.object.isRequired,
+};
--- src/components/IndexSection.jsx
+++ src/components/IndexSection.jsx
@@ -1,209 +1,82 @@
-import { useState } from "react";
-import { CanvasRevealEffect } from "../components/ui/CanvasRevealEffect";
-import Card from "../components/ui/Card";
-import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+import { CanvasRevealEffect } from '../components/ui/CanvasRevealEffect';
+import Card from '../components/ui/Card';
+import { Link } from 'react-router-dom';
+
+const cardsData = [
+  { title: 'INTRO', icon: '/index-icons/intro.svg', link: '#intro' },
+  { title: 'WEB', icon: '/index-icons/web.svg', link: '#web' },
+  { title: 'SOCIAL', icon: '/index-icons/social.svg', link: '#social' },
+  { title: 'PRINT', icon: '/index-icons/print.svg', link: '#print' },
+  { title: 'BRAND', icon: '/index-icons/brand.svg', link: '#brand' },
+  { title: 'EXTRA', icon: '/index-icons/extra.svg', link: '#extra' },
+  { title: 'CONTACT', icon: '/index-icons/contact.svg', link: '#contact' },
+];
+
+const colors = [
+  [236, 72, 153],
+  [232, 121, 249],
+];
+
+const CardComponent = ({ title, icon, link }) => (
+  <Card title={title} icon={icon} link={link}>
+    <CanvasRevealEffect
+      animationSpeed={3}
+      containerClassName='bg-black'
+      colors={colors}
+      dotSize={2}
+    />
+    <div className='absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90' />
+  </Card>
+);
+
+CardComponent.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+};
 
 export default function IndexSection({ elementsRef }) {
-  const [hovered, setHovered] = useState(false);
-
   return (
     <div
-      id="index"
-      data-name=""
+      id='index'
       ref={(el) => (elementsRef.current[1] = el)}
-      className="index-container">
+      className='index-container'
+    >
       <h2
-        data-option="fade-in"
-        className="fade-out index-container-heading animate">
-        As you scroll through, I'll take you along with me on a journey of
-        design excellence, showcase a few projects that I'm proud of, and in the
-        process, we'll get to know each other a little better.
+        data-option='fade-in'
+        className='fade-out index-container-heading animate'
+      >
+        As you scroll through, I&apos;ll take you along with me on a journey of
+        design excellence, showcase a few projects that I&apos;m proud of, and
+        in the process, we&apos;ll get to know each other a little better.
       </h2>
       <p
-        data-option="fade-in"
-        className="index-container-content fade-out animate">
-        Step-by-step let’s explore the possibilities, and if all the stars
-        align, our conversation will have actual words that we'll exchange over
-        a phone call.
+        data-option='fade-in'
+        className='index-container-content fade-out animate'
+      >
+        Step-by-step let&apos;s explore the possibilities, and if all the stars
+        align, our conversation will have actual words that we&apos;ll exchange
+        over a phone call.
       </p>
       <div
-        data-option="fade-in"
-        className="index-container-buttons fade-out animate">
+        data-option='fade-in'
+        className='index-container-buttons fade-out animate'
+      >
         <button>Brace Yourself!</button>
-        <a href="#intro">
+        <Link to='#intro'>
           <button>Who am I</button>
-        </a>
+        </Link>
       </div>
-      <div data-option="fade-in" className="index fade-out animate">
-        <Card title="INTRO" icon="/index-icons/intro.svg" link="#intro">
-          <CanvasRevealEffect
-            animationSpeed={3}
-            containerClassName="bg-black"
-            colors={[
-              [236, 72, 153],
-              [232, 121, 249],
-            ]}
-            dotSize={2}
-          />
-          <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
-        </Card>
-        <Card title="WEB" icon="/index-icons/web.svg" link="#web">
-          <CanvasRevealEffect
-            animationSpeed={3}
-            containerClassName="bg-black"
-            colors={[
-              [236, 72, 153],
-              [232, 121, 249],
-            ]}
-            dotSize={2}
-          />
-          <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
-        </Card>
-        <Card title="SOCIAL" icon="/index-icons/social.svg" link="#social">
-          <CanvasRevealEffect
-            animationSpeed={3}
-            containerClassName="bg-black"
-            colors={[
-              [236, 72, 153],
-              [232, 121, 249],
-            ]}
-            dotSize={2}
-          />
-          <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
-        </Card>
-        <Card title="PRINT" icon="/index-icons/print.svg" link="#print">
-          <CanvasRevealEffect
-            animationSpeed={3}
-            containerClassName="bg-black"
-            colors={[
-              [236, 72, 153],
-              [232, 121, 249],
-            ]}
-            dotSize={2}
-          />
-          <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
-        </Card>
-        <Card title="BRAND" icon="/index-icons/brand.svg" link="#brand">
-          <CanvasRevealEffect
-            animationSpeed={3}
-            containerClassName="bg-black"
-            colors={[
-              [236, 72, 153],
-              [232, 121, 249],
-            ]}
-            dotSize={2}
-          />
-          <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
-        </Card>
-        <Card title="EXTRA" icon="/index-icons/extra.svg" link="#extra">
-          <CanvasRevealEffect
-            animationSpeed={3}
-            containerClassName="bg-black"
-            colors={[
-              [236, 72, 153],
-              [232, 121, 249],
-            ]}
-            dotSize={2}
-          />
-          <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
-        </Card>
-        <Card title="CONTACT" icon="/index-icons/contact.svg" link="#contact">
-          <CanvasRevealEffect
-            animationSpeed={3}
-            containerClassName="bg-black"
-            colors={[
-              [236, 72, 153],
-              [232, 121, 249],
-            ]}
-            dotSize={2}
-          />
-          <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
-        </Card>
-        <Card title="INTRO" icon="/index-icons/intro.svg" link="#intro">
-          <CanvasRevealEffect
-            animationSpeed={3}
-            containerClassName="bg-black"
-            colors={[
-              [236, 72, 153],
-              [232, 121, 249],
-            ]}
-            dotSize={2}
-          />
-          <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
-        </Card>
-        <Card title="WEB" icon="/index-icons/web.svg" link="#web">
-          <CanvasRevealEffect
-            animationSpeed={3}
-            containerClassName="bg-black"
-            colors={[
-              [236, 72, 153],
-              [232, 121, 249],
-            ]}
-            dotSize={2}
-          />
-          <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
-        </Card>
-        <Card title="SOCIAL" icon="/index-icons/social.svg" link="#social">
-          <CanvasRevealEffect
-            animationSpeed={3}
-            containerClassName="bg-black"
-            colors={[
-              [236, 72, 153],
-              [232, 121, 249],
-            ]}
-            dotSize={2}
-          />
-          <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
-        </Card>
-        <Card title="PRINT" icon="/index-icons/print.svg" link="#print">
-          <CanvasRevealEffect
-            animationSpeed={3}
-            containerClassName="bg-black"
-            colors={[
-              [236, 72, 153],
-              [232, 121, 249],
-            ]}
-            dotSize={2}
-          />
-          <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
-        </Card>
-        <Card title="BRAND" icon="/index-icons/brand.svg" link="#brand">
-          <CanvasRevealEffect
-            animationSpeed={3}
-            containerClassName="bg-black"
-            colors={[
-              [236, 72, 153],
-              [232, 121, 249],
-            ]}
-            dotSize={2}
-          />
-          <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
-        </Card>
-        <Card title="EXTRA" icon="/index-icons/extra.svg" link="#extra">
-          <CanvasRevealEffect
-            animationSpeed={3}
-            containerClassName="bg-black"
-            colors={[
-              [236, 72, 153],
-              [232, 121, 249],
-            ]}
-            dotSize={2}
-          />
-          <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
-        </Card>
-        <Card title="CONTACT" icon="/index-icons/contact.svg" link="#contact">
-          <CanvasRevealEffect
-            animationSpeed={3}
-            containerClassName="bg-black"
-            colors={[
-              [236, 72, 153],
-              [232, 121, 249],
-            ]}
-            dotSize={2}
-          />
-          <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
-        </Card>
+      <div data-option='fade-in' className='index fade-out animate'>
+        {cardsData.map((card) => (
+          <CardComponent key={card.title} {...card} />
+        ))}
       </div>
     </div>
   );
 }
+
+IndexSection.propTypes = {
+  elementsRef: PropTypes.object.isRequired,
+};
